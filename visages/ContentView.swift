63550a29bb8003390515159b71e68bd5e714cb9a
hoangnguyen <hoangnguyen@blah>
2022-10-20 23:34:25 +0200

    Add navigation from ImageTile to ZoomableImageView

--- visages/ContentView.swift
+++ visages/ContentView.swift
@@ -12,21 +12,28 @@ struct ContentView: View {
     let scrolled = Axis.Set.horizontal
     let url = "https://cdn.posteritati.com/posters/000/000/060/465/fitzcarraldo-md-web.jpg"
     let urls = [
-        "https://s3.amazonaws.com/criterion-production/films/3617f85843446f8f78cc7ef44941a4c6/85USK613thTL36zAFZ4x1HtUfOG9NB_large.jpg",
-        "https://s3.amazonaws.com/criterion-production/films/587c8a3258addd59d3aa60ab4ab9e56b/zsEuDiXVAiOpAnXK08QtV7PfqZGbfX_large.jpg",
-        "https://s3.amazonaws.com/criterion-production/films/1461a0f7c135c79d89c6b20be956acaf/5WLjJmxnJwyZDlSormyyidLLosShkT_large.jpg",
-        "https://s3.amazonaws.com/criterion-production/films/323c56a13b44f5ded3b7b0117a6dc08d/o7l2a4KSto1HXIf7DxvRcPVn2sMRvl_large.jpg",
-        "https://s3.amazonaws.com/criterion-production/films/aacbf709cba74e607ae657b7ea6ff1cf/2jsGvFmN8GvcK82SeP32fOu0YpUx1i_large.jpg",
+        "https://s3.amazonaws.com/criterion-production/films/bd9c06388d59cfc3807d2d978d9b3113/1VhiMSCjj2iWXG4MW3xKDYZqO8sWc7_large.jpg",
+        "https://s3.amazonaws.com/criterion-production/films/46014912ca0c20592c472f5bbe33e4f8/L9AQ95Y7z2vOIUf1ntwvpZsZx3kp8B_large.jpg",
+        "https://s3.amazonaws.com/criterion-production/films/b7b640ae4949894b55998ff4983fc6e5/Et6yCL7xxsExwtRPO2aqmUHj1SnZcT_large.jpg",
+        "https://s3.amazonaws.com/criterion-production/films/64093139506eeea73f973aee07e3f9ec/ZTvHQrwEUsJIjlvtHztiZ6g0UCH85m_large.jpg",
+        "https://s3.amazonaws.com/criterion-production/films/8682fec5356a2cff41d1e090662f8a02/pvQjxjt452yR9hjwgX5DJ4UZeCraAL_large.jpg",
+        "https://s3.amazonaws.com/criterion-production/films/5e58f4effd7ec074436c3cefccc76e6f/Yp6qC4kg4JLjjqcNdK8RAVM8eK2qnR_large.jpg",
+        "https://s3.amazonaws.com/criterion-production/films/73a39f7f730d789ad75a85052fcb79ec/ctkavyALJzOHlnjyxyKzE5AyYFTLTJ_large.jpg",
+        "https://s3.amazonaws.com/criterion-production/films/c2dde679e36dd0dd23fd23cdf5d35bce/K48iXCNe4D2noqgK0VAWIIXZIuJShg_large.jpg"
+        
     ]
     var body: some View {
         
-        VStack {
+        NavigationView {
             ScrollView(.horizontal) {
-                LazyHGrid(rows: [GridItem(.adaptive(minimum: 300))]
+                LazyHGrid(rows: [GridItem(.adaptive(minimum: 100))]
                           //, spacing: 100
                 ) {
                     ForEach(urls, id: \.self) { url in
-                        ImageTile(url: url)
+                        NavigationLink(
+                            destination: ZoomableImageTile(url: url),
+                            label: { ImageTile(url: url) }
+                        )
                     }
                 }
             }
@@ -40,9 +47,10 @@ struct ZoomableImageTile: View {
     var body: some View {
         GeometryReader { proxy in
             AsyncImage(url: URL(string: url)) { image in
-                image.resizable()
+                image
+                    .resizable()
                     .frame(width: proxy.size.width, height: proxy.size.height)
-                    .scaledToFit()
+                    .scaledToFill()
                     .clipShape(Rectangle())
                     .modifier(ImageModifier(contentSize: CGSize(width: proxy.size.width, height: proxy.size.height)))
             } placeholder: {

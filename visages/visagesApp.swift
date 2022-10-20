aa378188639c6074343b38f817b608b9b7408de7
hoangnguyen <hoangnguyen@blah>
2022-10-20 20:36:08 +0200

    Initial Commit

---
+++ visages/visagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  visagesApp.swift
+//  visages
+//
+//  Created by Hoang Nguyen on 10/20/22.
+//
+
+import SwiftUI
+
+@main
+struct visagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
